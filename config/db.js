87753d0edf320b8cb3a2c54864ec68fd3e966a29
Jay Bisa <jaybisa@Jay-Bisas-MBP.hsd1.ma.comcast.net>
2016-07-10 14:33:15 -0400

    Move db connection code.

---
+++ config/db.js
@@ -0,0 +1,5 @@
+// Configure the db
+var databaseName = 'aunap_db';
+module.exports = {
+	url : 'mongodb://localhost/' + databaseName
+}
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-// Init express framework
-var express = require('express'),
-	path = require('path'),
-	mongoose = require('mongoose'),
-	bodyParser = require('body-parser'),
-	databaseName = 'aunap_db';
+// modules =================================================
+var express 	= require('express'),
+	path 		= require('path'),
+	mongoose 	= require('mongoose'),
+	bodyParser 	= require('body-parser');
 
-// Express setup
+// configuration ===========================================
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,17 +13,17 @@ app.use(express.static(path.join(__dirname + '/public')));
 // Routes set up
 var router = express.Router();
 
-mongoose.connect('mongodb://localhost/' + databaseName);
-
 // Connect to mongo db and start server only after sucessfully connecting
-var db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', startServer);
+var db = require('./config/db');
+mongoose.connect(db.url);
+var connection = mongoose.connection;
+connection.on('error', console.error);
+connection.once('open', startServer);
 
 // Start up server
 function startServer() {
 	var server = app.listen(3000, function() {
-	var port = server.address().port;
-	console.log('Server running on port ' + port);
-});
+		var port = server.address().port;
+		console.log('Server running on port ' + port);
+	});
 }
