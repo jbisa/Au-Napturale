257d62c7e58edb681e6129402c15642f4ebfcd93
Jay <jay.bisa@gmail.com>
2016-07-17 15:34:26 -0400

    Still not able to get all users from db.

---
+++ public/client/controllers/auNapCtrl.js
@@ -0,0 +1,16 @@
+/**
+ * Set up AngularJS app
+ */
+var myApp = angular.module('auNapApp', []);
+
+
+/**
+ * AngularJS controller which controls logic of app
+ */
+myApp.controller('AuNapCtrl', ['$scope', '$http', function($scope, $http) {
+
+  	$scope.saveEmailAddress = function() {
+  		console.log('***You entered: ' + $scope.customerEmail);
+  	};
+
+}]);
