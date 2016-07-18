f0c06134f97a8a3abb6c67a21a8f67756b0f8bef
Jay <jay.bisa@gmail.com>
2016-07-17 23:03:45 -0400

    Change messaging after the user registered.

--- public/client/controllers/auNapCtrl.js
+++ public/client/controllers/auNapCtrl.js
@@ -8,11 +8,15 @@ var myApp = angular.module('auNapApp', []);
  */
 myApp.controller('AuNapCtrl', ['$scope', '$http', function($scope, $http) {
 
+	$scope.registeredUser = false;
+
   	$scope.saveUser = function() {
   		// Save this user's email in db
   		$http.post('/add-user', {
   			email: $scope.customerEmail
   		});
+  		console.log('Inserted: ' + $scope.customerEmail);
+  		$scope.registeredUser = true;
   	};
 
 }]);
