e9184685b9952973d1e8e537854b673490f757c0
Jay <jay.bisa@gmail.com>
2016-07-24 15:44:09 -0400

    Configure heroku stuff.

--- config/db.js
+++ config/db.js
@@ -1,5 +1,14 @@
 // Configure the db
-var databaseName = 'aunap_db';
-module.exports = {
-	url : 'mongodb://localhost/' + databaseName
+var env = process.env.NODE_ENV;
+
+if (env === 'prod') {
+	module.exports = {
+		url : 'mongodb://jbisa:aunap123@ds029725.mlab.com:29725/aunap_db'
+	}
+} else {
+	var databaseName = 'aunap_db';
+	module.exports = {
+		url : 'mongodb://localhost/' + databaseName
+	}
 }
+
